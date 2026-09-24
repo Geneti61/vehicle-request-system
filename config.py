e5ff31d85fe24bf3974172cd0fca8e130089bf17
Geneti61 <gutetajenet1@gmail.com>
2026-09-24 11:42:02 +0300

    Add configuration for vehicles, warehouses, hubs, drivers, and users

---
+++ config.py
@@ -0,0 +1,55 @@
+# ============================================================
+# EPSS Vehicle System - Configuration
+# All drop-down data lives here
+# ============================================================
+
+VEHICLES = [
+    "IVECO NORMAL VAN",
+    "LOW BED",
+    "IVECO SMALL COLD VAN",
+    "TRUCK",
+    "HYUNDAI MIDIUM COLD VAN",
+    "IVECO EUROCARGO VAN NORMAL",
+    "IVECO VAN BIG",
+    "IVECO",
+    "OTHER",
+]
+
+WAREHOUSES = [
+    "M1", "M3", "M4", "M6", "M7", "M8", "M9",
+    "MODJO", "ACW", "SARIS", "Pharmacure", "sunsheng", "other",
+]
+
+HUBS = [
+    "Adama", "Hawasa", "Negele", "Arbaminch", "Jimma", "Gambela",
+    "Nekmte", "Asossa", "Bahrdar", "Gonder", "Dessie", "D Dawa",
+    "Jijiga", "Kebridar", "Semera", "Shire", "Mekele", "AA1", "AA2", "bole",
+]
+
+DRIVERS = [
+    "Abebe Fentahun - 4-23525",
+    "Abiyot Mekete - 4-18671",
+    "Aderamikael Tesfaye - 4-15056/00283",
+    "Adisu Negash - 4-35384",
+    "Afework Sahile - 4-23478",
+    "Alemayehu Asefa - 4-23505",
+    "Alemayehu Tadesse - 4-15055/00284",
+    "Alemu Seboka - 4-23578",
+    "Amare Ayele - 4-13499/00280",
+    "Antene Fantahun - 4-35378",
+    "Ashenafi Kebede - 4-14173/00282",
+    "Astawus Biriye - 4-18538",
+    "Ayele Debebe - 4-18050",
+    "Ayele Worku - 4-23477",
+    "Aysheshim Mulat - 4-23483",
+    "Bekele Kibret - 4-34291",
+    "Belay misganaw - 4-18667",
+    "Birhane Beza - 4-19952",
+]
+
+USERS = {
+    "gutejenet1@gmail.com":      {"password": "1234", "name": "Gguta",    "role": "Requester"},
+    "demekekerebih27@gmail.com": {"password": "1234", "name": "Dkerebih", "role": "Requester"},
+    "mergishoh@gmail.com":       {"password": "1234", "name": "Mhabtamu", "role": "Approver"},
+    "girmaabdeta@gmail.com":     {"password": "1234", "name": "Gabdeta",  "role": "Assigner"},
+}
